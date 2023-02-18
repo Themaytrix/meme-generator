eaaa9146fba7cd2f1ce04737bd58d65591bb3e00
Themaytrix <obengraymond81@gmail.com>
2023-02-18 22:51:58 +0000

    .

--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -1,11 +1,36 @@
+import React from "react"
+import memesData from "../memesData.js"
+
+
 export default function Meme(){
+    
+    // declaring states
+    const [meme, setMeme] = React.useState({
+        topText:"",
+        bottomText:"",
+        randomImage:"http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [memeImage,setMemeImage] = React.useState(memesData)
+
+
+    function getMemeImage(){
+        const memesArray = memeImage.data.memes
+        const randomNumber =Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+
+        setMeme((prevMeme)=>({...prevMeme,randomImage: url}))
+    }
     return(
         <main>
-            <form action="" className="form">
-                <input type="text" className="form--input"/>
-                <input type="text" className="form--input"/>
-                <input type="submit" value="generate meme!!"/>
-            </form>
+            <div className="form">
+                <input type="text" placeholder="Top text" className="form--input"/>
+                <input type="text" placeholder="Bottom text" className="form--input"/>
+               <button onClick={getMemeImage}  className="form--button">generate meme!!!</button>
+            </div>
+            <div className="meme">
+                <img  className="meme--image" src= {meme.randomImage} alt="" />
+            </div>
         </main>
     )
 }
